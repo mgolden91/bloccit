5402ae5887250f26dc232ea6f4145b578974a6d1
Matthew Golden <mgolden91@gmail.com>
2016-01-12 11:36:37 -0600

    Checkpoint 27 htmlcss complete

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,3 +1,6 @@
+
+
+
 // This is a manifest file that'll be compiled into application.js, which will include all the files
 // listed below.
 //
@@ -13,4 +16,5 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
+//= require bootstrap
 //= require_tree .
