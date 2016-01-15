01b99ce827307b118135539afaf973dddbd7e516
Matthew Golden <mgolden91@gmail.com>
2016-01-15 11:39:35 -0600

    Assignment-30-seedingdata complete

--- db/seeds.rb
+++ db/seeds.rb
@@ -19,6 +19,12 @@ end
 puts "Seed finished"
 puts "#{Post.count} posts created"
 puts "#{Comment.count} comments created"
+
+Post.find_or_create_by(title: "Donkey Lips was here")
+Comment.find_or_create_by(body: 'Here we are a new comment for you')
+Post.find_or_create_by(title: 'My name is Matt') do |user|
+  user.body = "here is the body"
+end
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
