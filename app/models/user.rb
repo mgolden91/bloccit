71c78fe1b5604930bcf222ba1c2481ee41639530
Matthew Golden <mgolden91@gmail.com>
2016-01-18 10:31:33 -0600

    Assignment-36-authentication complete

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,10 @@
 class User < ActiveRecord::Base
+
+
   before_save { self.email = email.downcase }
+  before_save :format_name
+
+
 
   EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
@@ -16,4 +21,14 @@ class User < ActiveRecord::Base
 
 
    has_secure_password
+
+   def format_name
+     if name
+       name_array = []
+       name.split.each do |x|
+         name_array << x.capitalize
+       end
+       self.name = name_array.join(" ")
+     end
+   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -27,6 +27,10 @@ RSpec.describe User, type: :model do
      it "should respond to email" do
        expect(user).to respond_to(:email)
      end
+
+     it "should format name" do
+       expect(user.name).to eq "Bloccit User"
+     end
    end
 
    describe "invalid user" do
