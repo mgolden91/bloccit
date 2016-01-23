94f4ad944f9a95a3bd41499f2369ec0ee09ed63b
Matthew Golden <mgolden91@gmail.com>
2016-01-23 13:17:22 -0600

    assignment-43-voting complete

--- app/models/post.rb
+++ app/models/post.rb
@@ -6,6 +6,8 @@ class Post < ActiveRecord::Base
   has_many :labelings, as: :labelable
   has_many :labels, through: :labelings
 
+  after_create :create_vote
+  
   default_scope { order('rank DESC') }
 
   validates :title, length: { minimum: 5 }, presence: true
@@ -31,4 +33,9 @@ class Post < ActiveRecord::Base
     update_attribute(:rank, new_rank)
   end
 
+  private
+
+  def create_vote
+    user.votes.create(value:1, post: self)
+  end
 end
