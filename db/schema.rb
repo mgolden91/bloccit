e7df19d7d70f510ff3f54a2bfd92ed9b2d44a749
Matthew Golden <mgolden91@gmail.com>
2016-01-23 12:25:39 -0600

    checkpoint-43-voting complete

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160122193359) do
+ActiveRecord::Schema.define(version: 20160123181035) do
 
   create_table "advertisements", force: :cascade do |t|
     t.string   "title"
@@ -56,6 +56,8 @@ ActiveRecord::Schema.define(version: 20160122193359) do
     t.datetime "updated_at", null: false
     t.integer  "topic_id"
     t.integer  "user_id"
+    t.integer  "rating"
+    t.float    "rank"
   end
 
   add_index "posts", ["topic_id"], name: "index_posts_on_topic_id"
@@ -65,6 +67,12 @@ ActiveRecord::Schema.define(version: 20160122193359) do
     t.string   "title"
     t.text     "body"
     t.boolean  "resolved"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "ratings", force: :cascade do |t|
+    t.integer  "severity"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -86,6 +94,7 @@ ActiveRecord::Schema.define(version: 20160122193359) do
     t.text     "description"
     t.datetime "created_at",                 null: false
     t.datetime "updated_at",                 null: false
+    t.integer  "rating"
   end
 
   create_table "users", force: :cascade do |t|
@@ -96,5 +105,16 @@ ActiveRecord::Schema.define(version: 20160122193359) do
     t.datetime "updated_at",      null: false
     t.integer  "role"
   end
+
+  create_table "votes", force: :cascade do |t|
+    t.integer  "value"
+    t.integer  "user_id"
+    t.integer  "post_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "votes", ["post_id"], name: "index_votes_on_post_id"
+  add_index "votes", ["user_id"], name: "index_votes_on_user_id"
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -18,12 +18,16 @@ include RandomData
  topics = Topic.all
 
 50.times do
-  Post.create!(
+  post = Post.create!(
     user: users.sample,
     topic: topics.sample,
     title: RandomData.random_sentence,
     body:  RandomData.random_paragraph
   )
+
+  post.update_attribute(:created_at, rand(10.minutes .. 1.year).ago)
+
+  rand(1..5).times { post.votes.create!(value: [-1, 1].sample, user: users.sample) }
 end
 posts = Post.all
 
@@ -55,6 +59,7 @@ puts "#{User.count} users created"
 puts "#{Topic.count} topics created"
 puts "#{Post.count} posts created"
 puts "#{Comment.count} comments created"
+puts "#{Vote.count} votes created"
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
