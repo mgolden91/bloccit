49a5102ba82b061ea85d74f53ea542a3e7344dae
Matthew Golden <mgolden91@gmail.com>
2016-01-24 11:17:28 -0600

    checkpoint-45-profiles complete

--- spec/controllers/favorites_controller_spec.rb
+++ spec/controllers/favorites_controller_spec.rb
@@ -4,10 +4,10 @@ include RandomData
 
 RSpec.describe FavoritesController, type: :controller do
 
-  let(:my_user) { User.create!(name: "Bloccit User", email: "user@bloccit.com", password: "helloworld") }
-  let(:my_topic) { Topic.create!(name:  RandomData.random_sentence, description: RandomData.random_paragraph) }
-  let(:my_post) { my_topic.posts.create!(title: RandomData.random_sentence, body: RandomData.random_paragraph, user: my_user) }
-
+  let(:my_topic) { create(:topic) }
+  let(:my_user) { create(:user) }
+  let(:my_post) { create(:post, topic: my_topic, user: my_user) }
+  
   context 'guest user' do
 
     describe 'POST create' do
@@ -44,7 +44,7 @@ RSpec.describe FavoritesController, type: :controller do
         expect(my_user.favorites.find_by_post_id(my_post.id)).not_to be_nil
       end
     end
-    
+
     describe 'DELETE destroy' do
 
       it 'redirects to the posts show view' do
